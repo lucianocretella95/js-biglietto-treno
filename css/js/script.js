56687f57acaa28b0a67036b56ae50c09bf704c05
lucianocretella95 <luciano.cretella@tim.it>
2024-04-17 17:04:19 +0200

    inizio if

--- css/js/script.js
+++ css/js/script.js
@@ -4,11 +4,12 @@ console.log(km);
 let age = prompt ("età passeggero");
 console.log(age);
 const prezzo = km * 0.21;
+let sconto20 = prezzo - (biglietto - (0.20 * 100));
 document.getElementById("standard").innerText= `il tuo biglietto costa: ${prezzo}`;
-// if (age <= 18) {
-//     // istruzione
-//   } else if (age >= 65) {
-//     // istruzione
-//   } else {
-//     // prezzo normale
-//   }
+if (age < 18) {
+    prezzo = prezzo - sconto20 ;
+  } else if (age >= 65) {
+    // istruzione
+  } else {
+    // prezzo normale
+  }
