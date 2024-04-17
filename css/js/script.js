65754b5802b66ec0b6276dc40046b3c861484ac4
lucianocretella95 <luciano.cretella@tim.it>
2024-04-17 16:42:10 +0200

    biglietto unico per tutti

--- css/js/script.js
+++ css/js/script.js
@@ -3,5 +3,12 @@ let km = prompt ("chilometri da percorrere");
 console.log(km);
 let age = prompt ("età passeggero");
 console.log(age);
-const prezzo = km * 0.21; 
+const prezzo = km * 0.21;
 document.getElementById("standard").innerText= `il tuo biglietto costa: ${prezzo}`;
+// if (age <= 18) {
+//     // istruzione
+//   } else if (age >= 65) {
+//     // istruzione
+//   } else {
+//     // prezzo normale
+//   }
