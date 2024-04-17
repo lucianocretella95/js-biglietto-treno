849feb60822a939677e6db42655ee31710560c36
lucianocretella95 <luciano.cretella@tim.it>
2024-04-17 17:26:41 +0200

    calcolo sconto

--- css/js/script.js
+++ css/js/script.js
@@ -4,8 +4,7 @@ console.log(km);
 let age = prompt ("età passeggero");
 console.log(age);
 const prezzo = km * 0.21;
-let sconto20 = prezzo - (biglietto - (0.20 * 100));
-document.getElementById("standard").innerText= `il tuo biglietto costa: ${prezzo}`;
+let sconto20 = km*0.21 - (0.21 - (0.20*100));
 if (age < 18) {
     prezzo = prezzo - sconto20 ;
   } else if (age >= 65) {
@@ -13,3 +12,4 @@ if (age < 18) {
   } else {
     // prezzo normale
   }
+document.getElementById("standard").innerText= `il tuo biglietto costa: ${prezzo}`;
